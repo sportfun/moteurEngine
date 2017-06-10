03f45d215be787bd2d217dbad47a4fc870fca34b
borner_c <clement.borner@epitech.com>
2017-06-10 17:20:04 +0200

    setup ES6 project adding src sources and update package.json

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * Created by JanJan on 10/06/2017.
+ */
+var nums = [1, 2, 3];
+var doubleNums = nums.map(function (e) {
+  return e * 2;
+});
---
+++ src/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by JanJan on 10/06/2017.
+ */
+var nums = [1,2,3];
+var doubleNums = nums.map((e) => e * 2);
