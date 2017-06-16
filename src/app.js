c01549405620e5885388b7c24754a7a1ed37a9a1
Victor Aubry <victor.aubry@epitech.eu>
2017-06-16 15:32:24 +0200

    Fix line endings

--- src/app.js
+++ src/app.js
@@ -56,100 +56,100 @@ function loadModels(path, texture, ext, pos, rot) {
     if (ext === '' || ext === undefined)
         extension = path.split('.')[1];
     let loaders = {
-      'obj': function (path, texture, pos, rot)
-      {
-          let   loader = new THREE.OBJLoader();
+        'obj': function (path, texture, pos, rot)
+        {
+            let   loader = new THREE.OBJLoader();
 
-          loader.load(
-              path,
-              function(object)
-              {
-                  if (texture !== undefined)
-                  {
-                      let textureLoader = THREE.TextureLoader;
-                      textureLoader.load(textures, function(textures)
-                      {
-                         let mat = new THREE.MeshBasicMaterial({map: textures});
-                      });
-                  }
-                  object.position = pos;
-                  object.rotation = rot;
-                  scene.add(object);
-              }
-          )
-      },
-      'dae': function (path, texture, pos, rot)
-      {
-          let   loader = new THREE.ColladaLoader();
+            loader.load(
+                path,
+                function(object)
+                {
+                    if (texture !== undefined)
+                    {
+                        let textureLoader = THREE.TextureLoader;
+                        textureLoader.load(textures, function(textures)
+                        {
+                            let mat = new THREE.MeshBasicMaterial({map: textures});
+                        });
+                    }
+                    object.position = pos;
+                    object.rotation = rot;
+                    scene.add(object);
+                }
+            );
+        },
+        'dae': function (path, texture, pos, rot)
+        {
+            let   loader = new THREE.ColladaLoader();
 
-          loader.load(
-              path,
-              function(object)
-              {
-                  if (texture !== undefined)
-                  {
-                      let textureLoader = THREE.TextureLoader;
-                      textureLoader.load(textures, function(textures)
-                      {
-                          let mat = new THREE.MeshBasicMaterial({map: textures});
-                      });
-                  }
-                  object.position = pos;
-                  object.rotation = rot;
-                  scene.add(object);
-              }
-          )
-      },
-      'fbx': function(path, texture, pos, rot)
-      {
-          let   loader = new THREE.FBXLoader();
+            loader.load(
+                path,
+                function(object)
+                {
+                    if (texture !== undefined)
+                    {
+                        let textureLoader = THREE.TextureLoader;
+                        textureLoader.load(textures, function(textures)
+                        {
+                            let mat = new THREE.MeshBasicMaterial({map: textures});
+                        });
+                    }
+                    object.position = pos;
+                    object.rotation = rot;
+                    scene.add(object);
+                }
+            );
+        },
+        'fbx': function(path, texture, pos, rot)
+        {
+            let   loader = new THREE.FBXLoader();
 
-          loader.load(
-              path,
-              function(object)
-              {
-                  if (texture !== undefined)
-                  {
-                      let textureLoader = THREE.TextureLoader;
-                      textureLoader.load(textures, function(textures)
-                      {
-                          let mat = new THREE.MeshBasicMaterial({map: textures});
-                      });
-                  }
-                  object.position = pos;
-                  object.rotation = rot;
-                  scene.add(object);
-              }
-          )
-      },
-    'json': function(path, texture, pos, rot)
-    {
-        let   loader = new THREE.FBXLoader();
+            loader.load(
+                path,
+                function(object)
+                {
+                    if (texture !== undefined)
+                    {
+                        let textureLoader = THREE.TextureLoader;
+                        textureLoader.load(textures, function(textures)
+                        {
+                            let mat = new THREE.MeshBasicMaterial({map: textures});
+                        });
+                    }
+                    object.position = pos;
+                    object.rotation = rot;
+                    scene.add(object);
+                }
+            );
+        },
+        'json': function(path, texture, pos, rot)
+        {
+            let   loader = new THREE.FBXLoader();
 
-        loader.load(
-            path,
-            function(object)
-            {
-                object.position = pos;
-                object.rotation = rot;
-                scene.add(object);
-            }
-        )
-    },
-    'js': function(path, texture, pos, rot)
-    {
-        let   loader = new THREE.FBXLoader();
+            loader.load(
+                path,
+                function(object)
+                {
+                    object.position = pos;
+                    object.rotation = rot;
+                    scene.add(object);
+                }
+            );
+        },
+        'js': function(path, texture, pos, rot)
+        {
+            let   loader = new THREE.FBXLoader();
 
-        loader.load(
-            path,
-            function(object)
-            {
-                object.position = pos;
-                object.rotation = rot;
-                scene.add(object);
-            }
-        )
-    }
+            loader.load(
+                path,
+                function(object)
+                {
+                    object.position = pos;
+                    object.rotation = rot;
+                    scene.add(object);
+                }
+            );
+        }
     };
     loaders[extension](path, texture, pos, rot);
 }
