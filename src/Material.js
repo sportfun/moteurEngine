36ebf8a2deb88b5d434b853f2cf7874503b515df
Victor Aubry <victor.aubry@epitech.eu>
2017-10-11 16:49:44 +0100

    Add basic Material class

---
+++ src/Material.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var THREE = require('three');
+
+var threeMaterialSymbol = Symbol();
+class Material {
+
+    get threeObject() {
+        return (this[threeMaterialSymbol]);
+    }
+
+    constructor() {
+        this[threeMaterialSymbol] = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    }
+
+    // param: number
+    // Set the opacity of the current material
+    // opacity should be between 0.0 (transparent) and 1.0 (opaque)
+    // If the transparent value of the Material is not set to true, this will have no effect
+    // 1.0 by default
+    SetOpacity(opacity) {
+        if (!isNaN(opacity)) {
+            this[threeMaterialSymbol].opacity = opacity;
+        }
+        else
+            console.log('Material: opacity value is not a Number');
+    }
+
+    // param: boolean
+    // Defines wether this material is transparent
+    // false by default
+    SetTransparent(transparent) {
+        this[threeMaterialSymbol].transparent = transparent;
+    }
+
+    // param: boolean
+    // Defines wether the material is affected by lights
+    // true by default
+    SetAffectedByLights(affected) {
+        this[threeMaterialSymbol].lights = affected;
+    }
+}
+
+export default Material;
